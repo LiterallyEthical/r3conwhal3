726739ecc304bd2cdce3bc2f6b18d3fd31b0962f
LiterallyEthical <ibolaski55@hotmail.com>
2024-03-07 14:49:36 +0300

    UPDATE indent

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -11,13 +11,15 @@ import (
 	"github.com/spf13/viper"
 )
 
-
 type Config struct {
-	OutDir string `mapstructure:"OUT_DIR"`
-	FileName string `mapstructure:"FILE_NAME"`
+	OutDir               string `mapstructure:"OUT_DIR"`
+	FileName             string `mapstructure:"FILE_NAME"`
 	Subkill3rWorkerCount string `mapstructure:"SUBKILL3R_WORKER_COUNT"`
-	Subkill3rServerAddr string `mapstructure:"SUBKILL3R_SERVER_ADDR"`
-	Subkill3rWordlist string `mapstructure:"SUBKILL3R_WORDLIST"`	
+	Subkill3rServerAddr  string `mapstructure:"SUBKILL3R_SERVER_ADDR"`
+	Subkill3rWordlist    string `mapstructure:"SUBKILL3R_WORDLIST"`
+	PurednsWordlist      string `mapstructure:"PUREDNS_WORDLIST"`
+	PurednsResolvers     string `mapstructure:"PUREDNS_RESOLVERS"`
+	PurednsNumOfThreads  int    `mapstructure:"PUREDNS_NUM_OF_THREADS"`
 }
 
 func LoadConfig(path string, docFS embed.FS) (config Config, err error) {
@@ -31,7 +33,7 @@ func LoadConfig(path string, docFS embed.FS) (config Config, err error) {
 		log.Fatalf(fmt.Sprintf("Couldn't find user's home directory, %v", err))
 		return
 	}
-	
+
 	// Set the default value for outDir
 	defaultDir := filepath.Join(homeDir, "r3conwhal3", "results")
 
@@ -41,13 +43,23 @@ func LoadConfig(path string, docFS embed.FS) (config Config, err error) {
 		log.Panic(err)
 	}
 
+	// Set the default path for puredns wordlist
+	puredns_wordlist, err := ExtractEmbeddedFileToTempDir(docFS, "docs/subdomains-top-110k.txt", "subdomains-top-110k.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	puredns_resolvers, err := ExtractEmbeddedFileToTempDir(docFS, "docs/resolvers.txt", "resolvers.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+
 	// Setting default values
 	viper.SetDefault("OUT_DIR", defaultDir)
-	viper.SetDefault("FILE_NAME", "subdomains.txt")	
+	viper.SetDefault("FILE_NAME", "subdomains.txt")
 	viper.SetDefault("SUBKILL3R_WORDLIST", subkill3r_wordlist)
-
-
-
+	viper.SetDefault("PUREDNS_WORDLIST", puredns_wordlist)
+	viper.SetDefault("PUREDNS_RESOLVERS", puredns_resolvers)
 
 	if path == "embedded" {
 		// Use the passed embedded FS to read the config file
