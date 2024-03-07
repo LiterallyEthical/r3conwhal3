26a93c95ab1f36b4ad577f9c427f5a95d0437bb0
LiterallyEthical <ibolaski55@hotmail.com>
2024-03-07 15:21:54 +0300

    UPDATE definitions and module calls

--- cmd/r3conwhal3/main.go
+++ cmd/r3conwhal3/main.go
@@ -18,16 +18,14 @@ import (
 )
 
 var (
-	cmds = []string{ "subfinder", "assetfinder", "amass", "httpx"}
+	cmds     = []string{"subfinder", "assetfinder", "amass", "httpx", "puredns"}
 	myLogger logger.Logger
 	//go:embed docs/*
-	docFS embed.FS
+	docFS          embed.FS
+	specifiedFiles []string
 )
 
-
-
 func main() {
-
 
 	// Accessing files from the embedded docs directory
 	data, err := fs.ReadFile(docFS, "docs/banner.txt")
@@ -38,47 +36,47 @@ func main() {
 
 	// Print the banner
 	fmt.Println(color.CyanString(string(data)))
-	
+
 	// Define flags
 	var domain, fileName, outDir, configDir string
 
-
 	pflag.StringVarP(&domain, "domain", "d", "", "Target domain to enumerate")
-    pflag.StringVarP(&configDir, "config-dir", "c", "embedded", "Path to directory which config.env exists")
-    pflag.StringVarP(&outDir, "out-dir", "o", "$HOME/user/r3conwhal3/results", "Directory to keep all output")
-    pflag.StringVarP(&fileName, "file-name", "f", "subdomains.txt", "File to write subdomains")
+	pflag.StringVarP(&configDir, "config-dir", "c", "embedded", "Path to directory which config.env exists")
+	pflag.StringVarP(&outDir, "out-dir", "o", "$HOME/user/r3conwhal3/results", "Directory to keep all output")
+	pflag.StringVarP(&fileName, "file-name", "f", "subdomains.txt", "File to write subdomains")
 	pflag.Parse()
-
 
 	config, err := utils.LoadConfig(configDir, docFS)
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
 
-    // Set the flag value from the config if not explicitly set via command line
-    if !pflag.Lookup("out-dir").Changed {
+	// Set the flag value from the config if not explicitly set via command line
+	if !pflag.Lookup("out-dir").Changed {
 		// Get the value from config, if flag is not set
-        outDir = viper.GetString("OUT_DIR") 
-    }
+		outDir = viper.GetString("OUT_DIR")
+	}
 
 	if !pflag.Lookup("file-name").Changed {
-        fileName = viper.GetString("FILE_NAME") 
-    }
+		fileName = viper.GetString("FILE_NAME")
+	}
 
-	// Binding variables from config.env to flags	
+	// Binding variables from config.env to flags
 	viper.BindPFlag("OUT_DIR", pflag.Lookup("out-dir"))
 	viper.BindPFlag("FILE_NAME", pflag.Lookup("file-name"))
-
 
 	// Define variables for subkill3r
 	workerCount, err := strconv.Atoi(config.Subkill3rWorkerCount)
 	if err != nil {
-		panic(err)
-		//myLogger.Error("error running subkill3r: workerCount is type string instead of int" , err)
+		myLogger.Error("error running subkill3r: workerCount is type string instead of int", err)
 	}
 	serverAddr := config.Subkill3rServerAddr
 	wordlist := config.Subkill3rWordlist
 
+	// Define variables for puredns
+	purednsWordlist := config.PurednsWordlist
+	purednsResolvers := config.PurednsResolvers
+	purednsNumOfThreads := config.PurednsNumOfThreads
 
 	// Check if the domain is provided or not
 	if domain == "" {
@@ -92,16 +90,26 @@ func main() {
 	}
 
 	// Create directory to keep all output
-	dirPath, err := utils.CreateDir(outDir, domain)
+	outDirPath, err := utils.CreateDir(outDir, domain)
 	if err != nil {
 		myLogger.Error("Failed to create directory: %v, %v", outDir, err)
 	}
+	// Join full path for activeSubdEnum
+	activeFileName := "active_enum_subdomains.txt"
+	activeFilePath := path.Join(outDirPath, activeFileName)
+	passiveFileName := "passive_enum_subdomains.txt"
+	passiveFilePath := path.Join(outDirPath, passiveFileName)
+	specifiedFiles = append(specifiedFiles, passiveFileName, activeFileName)
 
-	// Join full path
-	filePath := path.Join(dirPath, fileName)
-	
-	if err := mods.InitSubdEnum(domain, filePath, dirPath, wordlist, serverAddr, workerCount); err != nil {
+	if err := mods.InitSubdEnum(domain, passiveFilePath, outDirPath, wordlist, serverAddr, workerCount); err != nil {
 		log.Fatal(err)
 	}
 
+	if err := mods.InitActiveSubdEnum(outDirPath, domain, activeFilePath, purednsWordlist, purednsResolvers, purednsNumOfThreads); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := mods.InitFilterLiveDomains(outDirPath, specifiedFiles); err != nil {
+		log.Fatal(err)
+	}
 }
